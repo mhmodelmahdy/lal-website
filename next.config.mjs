f51db7118c1c939dd26bbd6f2388883a0751f634
mhmodelmahdy <mahmoudelmahdy208@gmail.com>
2026-02-18 17:39:11 +0200

    تعديل

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,11 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "images.unsplash.com" }
-    ]
-  }
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "drive.google.com" },
+      { protocol: "https", hostname: "lh3.googleusercontent.com" },
+    ],
+  },
 };
 
 export default nextConfig;
